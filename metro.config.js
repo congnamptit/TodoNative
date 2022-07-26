eeb522804b6362f985ccb80512cf58af503be809
namnc <namnc@skydart.app>
2022-07-26 09:22:26 +0700

    add EditTodoView.tsx

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,8 @@
  */
 
 module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
